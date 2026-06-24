a31f0ba41e4babe00a17cb5435ec023afcd6e998
Anilabha Datta <anilabhadatta@gmail.com>
2026-06-25 01:34:34 +0530

    build fix

--- composeApp/src/iosFull/kotlin/com/nuvio/app/core/build/AppFeaturePolicy.ios.kt
+++ composeApp/src/iosFull/kotlin/com/nuvio/app/core/build/AppFeaturePolicy.ios.kt
@@ -3,8 +3,8 @@ package com.nuvio.app.core.build
 actual object AppFeaturePolicy {
     actual val pluginsEnabled: Boolean = true
     actual val supportersContributorsPageEnabled: Boolean = true
-    actual val accountDeletionEnabled: Boolean = false
-    actual val personalMediaAddonCopyEnabled: Boolean = false
+    actual val accountDeletionEnabled: Boolean = true
+    actual val personalMediaAddonCopyEnabled: Boolean = true
     actual val p2pEnabled: Boolean = false
     actual val trailerPlaybackMode: TrailerPlaybackMode = TrailerPlaybackMode.IN_APP
     actual val heroTrailerPlaybackSupported: Boolean = false
